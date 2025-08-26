312c7be2ae60a74290c50cb44aad28dff3a5a1e3
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-08-26 17:06:11 -0300

    feat: add home page content

--- apps/docs/app/[locale]/page.tsx
+++ apps/docs/app/[locale]/page.tsx
@@ -1,6 +1,12 @@
+// Hooks
+import { getTranslations } from 'next-intl/server';
+
+// Utils
 import { setRequestLocale } from 'next-intl/server';
 
-// Sections
+// Components
+import { Link } from '@/i18n/navigation';
+import { ArrowRightIcon } from '@radix-ui/react-icons';
 
 type PageProps = {
   params: Promise<{
@@ -10,8 +16,27 @@ type PageProps = {
 
 export default async function HomePage({ params }: PageProps) {
   const { locale } = await params;
+  const t = await getTranslations('sections');
+  const actions = t.raw('home.actions');
 
   setRequestLocale(locale);
 
-  return <></>;
+  return (
+    <section className="mx-auto flex min-h-lvh max-w-xl flex-col justify-center gap-y-3">
+      <h1 className="text-xl">
+        <span className="font-bold">I7A UI</span> - React UI Kit
+      </h1>
+      <p className="text-muted-foreground text-sm">{t('home.description')}</p>
+      {actions.map((action: { label: string; href: string }) => (
+        <Link
+          href={action.href}
+          key={action.href}
+          className="text-primary flex items-center gap-x-2 text-sm underline"
+        >
+          {action.label}
+          <ArrowRightIcon />
+        </Link>
+      ))}
+    </section>
+  );
 }
