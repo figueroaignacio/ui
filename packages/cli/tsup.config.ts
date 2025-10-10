2cafc94659a9790f3e6ba8201607ca7dc64b9531
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-10-10 08:11:28 -0300

    chore: some changes to cli

--- packages/cli/tsup.config.ts
+++ packages/cli/tsup.config.ts
@@ -3,11 +3,12 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   entry: ['src/cli.ts'],
   format: ['esm'],
-  outDir: 'dist',
   clean: true,
-  target: 'es2020',
-  sourcemap: true,
-  banner: {
-    js: '#!/usr/bin/env node',
+  dts: false,
+  outDir: 'dist',
+  esbuildOptions(options) {
+    options.banner = {
+      js: '#!/usr/bin/env node',
+    };
   },
 });
