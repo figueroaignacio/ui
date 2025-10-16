be32944dea4e22ce5e6795e49b93e9a6ed247d96
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-10-16 18:36:41 -0300

    feat: add base project

--- apps/docs/components/logo.tsx
+++
@@ -1,5 +0,0 @@
-import { Link } from '@/i18n/navigation';
-
-export function Logo() {
-  return <Link href="/">I7A UI</Link>;
-}
--- apps/docs/i18n/namespaces.ts
+++
@@ -1,1 +0,0 @@
-export const namespaces: string[] = ['ui', 'sections', 'components'];
