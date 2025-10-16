c6f00e9d34d6239be7aa35e72218f8b9cf7f087a
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-10-16 10:53:52 -0300

    fix: import

--- apps/docs/components/github-link.tsx
+++ apps/docs/components/github-link.tsx
@@ -3,7 +3,7 @@ import { Link } from '@/i18n/navigation';
 import { GitHubLogoIcon } from '@radix-ui/react-icons';
 
 // Utils
-import { buttonVariants } from './ui/button';
+import { buttonVariants } from '@repo/ui/components/button';
 
 export function GitHubLink() {
   return (
