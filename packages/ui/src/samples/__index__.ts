959782c0d69a867652082acb219d81e8d10cdcbe
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-10-23 06:47:05 -0300

    feat: move samples to ui package

---
+++ packages/ui/src/samples/__index__.ts
@@ -0,0 +1,9 @@
+import { ButtonDemo } from './components/button-demo';
+
+export const Index = {
+  'button-demo': {
+    name: 'button-demo',
+    component: ButtonDemo,
+    file: 'packages/ui/src/samples/components/button-demo.tsx',
+  },
+};
---
+++ packages/ui/src/samples/components/button-demo.tsx
@@ -0,0 +1,5 @@
+import { Button } from '../../components/button';
+
+export function ButtonDemo() {
+  return <Button>Click me</Button>;
+}
