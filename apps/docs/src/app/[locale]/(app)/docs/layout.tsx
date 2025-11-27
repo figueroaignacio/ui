85385ead5e2a5f975813b1b75e654f53254f9220
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-11-26 21:22:40 -0300

    feat: remove bg blur by now

--- apps/docs/src/app/[locale]/(app)/docs/layout.tsx
+++ apps/docs/src/app/[locale]/(app)/docs/layout.tsx
@@ -1,11 +1,9 @@
 // Components
-import { BgBlur } from '@/components/bg-blur';
 import { Sidebar } from '@/components/sidebar';
 
 export default function DocsLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="relative grid grid-cols-1 gap-3 lg:grid-cols-[210px_1fr_210px]">
-      <BgBlur />
       <div>
         <Sidebar />
       </div>
