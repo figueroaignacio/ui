cdfd03859ad07d69fada5734cc00cc011d32bb54
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-12-03 18:47:31 -0300

    feat: add blog page

--- apps/docs/src/features/blog/lib/get-posts.ts
+++ apps/docs/src/features/blog/lib/get-posts.ts
@@ -1,5 +1,5 @@
-import { posts } from "@/content";
+import { posts } from '@/content';
 
 export function getPosts(locale: string) {
-  return posts.filter((post) => post.locale === locale)
+  return posts.filter((post) => post.locale === locale);
 }
