477d42003f2727da6d399cf41e12601f98bdae4f
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-12-05 10:05:40 -0300

    feat: remove viewTransitions in next.config.ts

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -6,9 +6,6 @@ const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig: NextConfig = {
   typedRoutes: true,
-  experimental: {
-    viewTransition: true,
-  },
   outputFileTracingIncludes: {
     '/': ['../../packages/ui/src/samples/**'],
   },
