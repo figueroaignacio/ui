4db151b75fd09c67c523dee0ee03e01c4cd1e8fd
figueroaignacio <ignaciofigueroadev@gmail.com>
2026-02-10 18:45:15 -0300

    feat: change table styles

--- packages/ui/src/components/table.tsx
+++ packages/ui/src/components/table.tsx
@@ -5,7 +5,7 @@ import * as React from 'react';
 
 const Table = React.forwardRef<HTMLTableElement, React.HTMLAttributes<HTMLTableElement>>(
   ({ className, ...props }, ref) => (
-    <div className="relative w-full overflow-auto rounded-lg">
+    <div className="border-border relative w-full overflow-auto rounded-md border">
       <table ref={ref} className={cn('w-full caption-bottom text-sm', className)} {...props} />
     </div>
   ),
