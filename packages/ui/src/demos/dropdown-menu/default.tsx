bba4100eeae3b0758d8d68bf2caa55e12cfed117
figueroaignacio <ignaciofigueroadev@gmail.com>
2026-03-19 18:54:43 -0300

    fix: dropdown menu default demo icons

--- packages/ui/src/demos/dropdown-menu/default.tsx
+++ packages/ui/src/demos/dropdown-menu/default.tsx
@@ -1,7 +1,14 @@
 'use client';
 
-import { Bell, Keyboard, LogOut, Settings, Shield, User } from 'lucide-react';
-
+import {
+  KeyboardIcon,
+  Logout02Icon,
+  Notification03Icon,
+  Settings01Icon,
+  Shield02Icon,
+  UserIcon,
+} from '@hugeicons/core-free-icons';
+import { HugeiconsIcon } from '@hugeicons/react';
 import { Button } from '../../components/button';
 import { DropdownMenu } from '../../components/dropdown-menu';
 
@@ -14,30 +21,30 @@ export function Default() {
       <DropdownMenu.Content className="w-56" align="start">
         <DropdownMenu.Label>Account</DropdownMenu.Label>
         <DropdownMenu.Item>
-          <User className="mr-2 size-4" />
+          <HugeiconsIcon icon={UserIcon} className="mr-2 size-4" />
           Profile
         </DropdownMenu.Item>
         <DropdownMenu.Item>
-          <Bell className="mr-2 size-4" />
+          <HugeiconsIcon icon={Notification03Icon} className="mr-2 size-4" />
           Notifications
         </DropdownMenu.Item>
         <DropdownMenu.Item>
-          <Settings className="mr-2 size-4" />
+          <HugeiconsIcon icon={Settings01Icon} className="mr-2 size-4" />
           Settings
         </DropdownMenu.Item>
         <DropdownMenu.Separator />
         <DropdownMenu.Label>Security</DropdownMenu.Label>
         <DropdownMenu.Item>
-          <Shield className="mr-2 size-4" />
+          <HugeiconsIcon icon={Shield02Icon} className="mr-2 size-4" />
           Privacy & Security
         </DropdownMenu.Item>
         <DropdownMenu.Item>
-          <Keyboard className="mr-2 size-4" />
+          <HugeiconsIcon icon={KeyboardIcon} className="mr-2 size-4" />
           Keyboard shortcuts
         </DropdownMenu.Item>
         <DropdownMenu.Separator />
         <DropdownMenu.Item className="text-destructive focus:text-destructive">
-          <LogOut className="mr-2 size-4" />
+          <HugeiconsIcon icon={Logout02Icon} className="mr-2 size-4" />
           Sign out
         </DropdownMenu.Item>
       </DropdownMenu.Content>
