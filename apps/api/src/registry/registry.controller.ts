18b9d01e191aea22578381ec956b554360375609
figueroaignacio <ignaciofigueroadev@gmail.com>
2026-04-08 18:28:56 -0300

    feat: add api registry resource

---
+++ apps/api/src/registry/registry.controller.ts
@@ -0,0 +1,7 @@
+import { Controller } from '@nestjs/common';
+import { RegistryService } from './registry.service';
+
+@Controller('registry')
+export class RegistryController {
+  constructor(private readonly registryService: RegistryService) {}
+}
