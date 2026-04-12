fc0990449ad93f173ff24fbd5048814e969ea351
figueroaignacio <ignaciofigueroadev@gmail.com>
2026-04-11 23:05:35 -0300

    chore(api): create modules folder to keep it modular

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RegistryModule } from './registry/registry.module';
-import { ThemesModule } from './themes/themes.module';
+import { RegistryModule } from './modules/registry/registry.module';
+import { ThemesModule } from './modules/themes/themes.module';
 
 @Module({
   imports: [RegistryModule, ThemesModule],
