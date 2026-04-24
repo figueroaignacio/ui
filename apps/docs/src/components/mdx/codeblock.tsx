b0e35ca83ad4e7ff6edd6f0fa5350af7d550dd5f
figueroaignacio <ignaciofigueroadev@gmail.com>
2026-04-24 20:22:04 -0300

    fix(docs): fix codeblock bg color in light mode

--- apps/docs/src/components/mdx/codeblock.tsx
+++ apps/docs/src/components/mdx/codeblock.tsx
@@ -51,7 +51,7 @@ export function CodeBlock({
   return (
     <div
       className={cn(
-        'group relative mt-6 w-full overflow-hidden rounded-md bg-[#1e1f20]/50 transition-all duration-500',
+        'group relative mt-6 w-full overflow-hidden rounded-md bg-[#1e1f20] transition-all duration-500',
         !internalIsExpanded ? 'max-h-[450px]' : 'max-h-none',
         className,
       )}
